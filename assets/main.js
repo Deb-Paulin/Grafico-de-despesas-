ae2d72098e4a41737c517e63ca5008f86cae56c6
Débora Paulin <113848968+Deb-Paulin@users.noreply.github.com>
2023-03-16 11:07:13 -0300

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,49 @@
+//col_0
+document.getElementById("coluna_0").addEventListener("mousedown", () => {
+    document.getElementById("val0").id = "value";
+  });
+  document.getElementById("coluna_0").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val0";
+  });
+  //col_1
+  document.getElementById("coluna_1").addEventListener("mousedown", () => {
+    document.getElementById("val1").id = "value";
+  });
+  document.getElementById("coluna_1").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val1";
+  });
+  //col_2
+  document.getElementById("coluna_2").addEventListener("mousedown", () => {
+    document.getElementById("val2").id = "value";
+  });
+  document.getElementById("coluna_2").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val2";
+  });
+  //col_3
+  document.getElementById("coluna_3").addEventListener("mousedown", () => {
+    document.getElementById("val3").id = "value";
+  });
+  document.getElementById("coluna_3").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val3";
+  });
+  //col_4
+  document.getElementById("coluna_4").addEventListener("mousedown", () => {
+    document.getElementById("val4").id = "value";
+  });
+  document.getElementById("coluna_4").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val4";
+  });
+  //col_5
+  document.getElementById("coluna_5").addEventListener("mousedown", () => {
+    document.getElementById("val5").id = "value";
+  });
+  document.getElementById("coluna_5").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val5";
+  });
+  //col_6
+  document.getElementById("coluna_6").addEventListener("mousedown", () => {
+    document.getElementById("val6").id = "value";
+  });
+  document.getElementById("coluna_6").addEventListener("mouseup", () => {
+    document.getElementById("value").id = "val6";
+  });
